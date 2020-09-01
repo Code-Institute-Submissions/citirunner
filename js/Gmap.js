9f21f98c1a8adc3da425e77acbd96941a784fd2a
mr19331 <mr19331@gmail.com>
2020-09-01 10:03:07 +0000

    http.server

--- js/Gmap.js
+++ js/Gmap.js
@@ -3,6 +3,6 @@ function initMap() {
   // Constructor creates a new map
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 40.7625587, lng: -73.9733541 },
-    zoom: 10,
+    zoom: 15,
   });
 }
