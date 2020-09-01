788b8eb2fa470720282c9723026d981c856e6fb5
mr19331 <mr19331@gmail.com>
2020-09-01 09:48:42 +0000

    add Gmap Api & code

---
+++ js/Gmap.js
@@ -0,0 +1,8 @@
+var map;
+function initMap() {
+  // Constructor creates a new map
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.7625587, lng: -73.9733541 },
+    zoom: 10,
+  });
+}
