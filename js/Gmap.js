65e84e98ae8b97987b2bdca011dd382ec51dd9f3
mr19331 <mr19331@gmail.com>
2020-09-02 06:55:32 +0000

    add markers,infowindws to Gmap

--- js/Gmap.js
+++ js/Gmap.js
@@ -1,8 +1,27 @@
-var map;
+
+let map;
 function initMap() {
-  // Constructor creates a new map
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.7625587, lng: -73.9733541 },
-    zoom: 15,
-  });
+	map = new google.maps.Map( document.getElementById( 'map' ), {
+		center: {
+			lat: 40.7625587,
+			lng: -73.9733541
+		},
+		zoom: 14
+    });
+
+
+ let marker = new google.maps.Marker({
+	position: {
+		lat: 40.7625587,
+		lng: -73.9733541
+	},
+	map: map,
+	title: '<h2>Your Hotel Location'</h2>
+});
+let infoWindow = new google.maps.InfoWindow ({
+    content: 'Vista Gym Club NY'
+});
+marker.addListener('click',function(){
+  infoWindow.open( map,marker);  
+});
 }
