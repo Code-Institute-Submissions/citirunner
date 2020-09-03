5fae20c65f593539e5b6af02ddd529b0266f6fa9
mr19331 <mr19331@gmail.com>
2020-09-03 11:16:53 +0000

    add multimarkers & infow

--- js/Gmap.js
+++ js/Gmap.js
@@ -1,30 +1,82 @@
-let map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 40.7623515,
-      lng: -73.9718842,
-    },
-    zoom: 17,
-    mapTypeId: "roadmap",
+  var center = {lat: 40.7625587, lng: -73.9733541};
+  
+  var locations = [
+    ['Belvista Hotel<br>\
+    57 E 57th St, New York, NY 10022, United States<br>\
+   <a href="https://www.fourseasons.com/newyork/">Website</a>',40.7625587, -73.9733541],
+    ['New York Sports Club<br>\
+    502 Park Ave New York NY 10022<br>\
+   <a href="https://www.newyorksportsclubs.com/clubs/59th-park">Website</a>',40.7617002, -73.9773837],
+    ['Velocity Sports NYC <br>\
+    133 E 58th St 6th floor, New York, NY 10022<br>\
+    <a href="https://goo.gl/maps/eUmyNuMyYNN2">Website</a>', 40.762066, -73.9710607],
+    ['Lift Gym<br>\
+    139 E 57th St New York NY 10022<br>\
+    <a href="http://liftgym.com/">Website</a>', 40.7576454,-73.9722553 ],
+    ['Vital Yoga<br>\
+    212 E 57th St,  New York NY 10022<br>\
+   <a href="https://halevylife.com/vital-yoga-nyc/">Website</a>', 40.7599039, -73.9693033]
+  ];
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: center
   });
-
-  let marker = new google.maps.Marker({
-    position: {
-      lat: 40.7623515,
-      lng: -73.9718842,
-    },
-    map: map,
-    title: "Your Location",
-  });
-  let infoWindow = new google.maps.InfoWindow({
-    content: "<h2>Your Hotel Location!</h2>",
-  });
-
-  marker.addListener("click", function () {
-    infoWindow.open(map, marker);
-  });
+var infowindow =  new google.maps.InfoWindow({});
+var marker, count;
+for (count = 0; count < locations.length; count++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+      map: map,
+      title: locations[count][0]
+    });
+google.maps.event.addListener(marker,'click', (function (marker, count) {
+      return function () {
+        infowindow.setContent(locations[count][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, count));
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+// let map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: {
+//       lat: 40.7623515,
+//       lng: -73.9718842,
+//     },
+//     zoom: 17,
+//     mapTypeId: "roadmap",
+//   });
+
+//   let marker = new google.maps.Marker({
+//     position: {
+//       lat: 40.7623515,
+//       lng: -73.9718842,
+//     },
+//     map: map,
+//     title: "Your Location",
+//   });
+//   let infoWindow = new google.maps.InfoWindow({
+//     content: "<h2>Your Hotel Location!</h2>",
+//   });
+
+//   marker.addListener("click", function () {
+//     infoWindow.open(map, marker);
+//   });
+// }
 
 // let map;
 // function initMap() {
