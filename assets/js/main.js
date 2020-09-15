127278ef6c48a544b40a3786ca04a7ed8091ef0f
mr19331 <mr19331@gmail.com>
2020-09-15 09:43:07 +0000

    position contact header

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-   
-   
- $(document).ready(function(){
-        $('.thumb a').click(function(e){
-           e.preventDefault();
-             $('.imgBox img').attr("src", $(this).attr("href")); 
- });
-});
