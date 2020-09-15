cb5806d3b4f4169e33af637ccb3cfb623be80488
mr19331 <mr19331@gmail.com>
2020-09-15 15:08:38 +0000

    add code & imgs pt section

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,16 +1,23 @@
+const notificationElement = document.querySelector("#message");
+
 function sendMail(contactForm){
   emailjs.send("gmail","gymdiscount",{
        "from_name": contactForm.name.value,
    "from_email":contactForm.emailaddress.value
   
 })
-.then(
-       function(response) {
-           console.log("SUCCESS", response);
-       },
-       function(error) {
-           console.log("FAILED", error);
+  .then(
+       function (response) {
+            alert("SUCCESS", response);
+
+   }
+
+        },
+        function(error) {
+            alert("FAILED", error);
        }
-   );
+
+    );
+
    return false;  // To block from loading a new page
 }
